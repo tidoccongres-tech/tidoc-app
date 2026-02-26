d1afeb2c2634a6859d7a4dff95a4dd15efcc618f
tidoccongres-tech <tidoc.congres@gmail.com>
2026-02-26 08:33:49 +0200

    Update billets.js

--- www/billets.js
+++ www/billets.js
@@ -11,9 +11,6 @@ import {
   doc, getDoc, setDoc, updateDoc, deleteDoc, runTransaction,
   serverTimestamp, collection, query, where, getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
-
-import { initFocusMode } from "./ui-fullscreen.js";
-initFocusMode();
 // =====================
 // UI
 // =====================
