29031d5db2d492989d5cd6021ed7b1c4bd902bbe
tidoccongres-tech <tidoc.congres@gmail.com>
2026-02-26 08:36:21 +0200

    Update notifications.js

--- www/notifications.js
+++ www/notifications.js
@@ -14,9 +14,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
-import { initFocusMode } from "./ui-fullscreen.js";
-initFocusMode();
-
 // ===== Cloudinary (newsletter uploads) =====
 const CLOUD_NAME = "dctwkkvn1";
 const UPLOAD_PRESET_NEWSLETTER = "tidoc_galerie";
